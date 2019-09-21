834b635e5a0002818b1039b368821fe6adfb6ad3
Katarzyna Gawrys <katarzyna.gawrys@polsl.pl>
2019-09-21 12:30:18 +0200

    zmiana main w projekt2

--- src/pl/java/Main.java
+++ src/pl/java/Main.java
@@ -3,6 +3,7 @@ package pl.java;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello  - umieść mnie na GitHub'ie");
+
+        System.out.println("Hello  - jestem klonem");
     }
 }
